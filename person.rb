5968aa43f737d9cdae3881394ed7224d3017c6e2
Nicolae Pop <nicupop729@gmail.com>
2022-01-24 20:32:13 -0300

    feat: build class Person

---
+++ person.rb
@@ -0,0 +1,24 @@
+class Person
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_accessor :name
+  attr_accessor :age
+  attr_reader :id
+
+  def can_use_services?
+    true if is_of_age? || @parent_permission
+  end
+
+  private
+
+  def is_of_age?
+    true if @age >= 18
+   
+  end
+
+end
