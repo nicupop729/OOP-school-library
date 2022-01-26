4d2548a85e61c9d646f0a5fc23a4ce5a91f78a21
Nicolae Pop <nicupop729@gmail.com>
2022-01-26 19:54:59 -0300

    feat: create the class Rental

---
+++ rental.rb
@@ -0,0 +1,20 @@
+class Rental
+  attr_accessor :date
+  attr_reader :book, :person
+  
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+  end
+
+  def book=(book)
+    @book = book
+    book.rental.push(self) unless book.rental.include?(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.rental.push(self) unless person.rental.include?(self)
+  end
+end
