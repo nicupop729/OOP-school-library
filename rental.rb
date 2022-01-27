8bd69a862e0ee041d6c279c4a49d135244a45924
Nicolae Pop <nicupop729@gmail.com>
2022-01-26 21:29:50 -0300

    fix: refactor with CR sugestions

--- rental.rb
+++ rental.rb
@@ -1,20 +1,9 @@
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
+  attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
-  end
-
-  def book=(book)
-    @book = book
-    book.rental.push(self) unless book.rental.include?(self)
-  end
-
-  def person=(person)
-    @person = person
-    person.rental.push(self) unless person.rental.include?(self)
   end
 end
